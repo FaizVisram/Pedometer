97a3b4757e86b9e9ab7fa350060f605fbf0abe9f
Faiz Visram <faiz.visram@gmail.com>
2012-09-05 20:35:08 -0400

    Removed whitespace errors

--- src/com/faizvisram/android/Pedometer/NotificationHandler.java
+++ src/com/faizvisram/android/Pedometer/NotificationHandler.java
@@ -3,10 +3,34 @@
  */
 package com.faizvisram.android.Pedometer;
 
+import android.app.NotificationManager;
+import android.content.Context;
+
 /**
  * @author Faiz
  *
  */
 public class NotificationHandler {
+	private static NotificationHandler mInstance;
+	private Context mContext;
+	private NotificationManager mNM;
 
+	private NotificationHandler() {
+		mInstance = this;
+
+	}
+
+	public NotificationHandler getInstance(Context context) {
+		if (context != null)
+			mContext = context;
+
+		if (mInstance == null)
+			mInstance = new NotificationHandler();
+
+		return mInstance;
+	}
+
+	public void updateNotification() {
+
+	}
 }
